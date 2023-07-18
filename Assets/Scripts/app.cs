dd28d9215c155e4f4cfe0fda9fc15a2335b023ac
Özcan Demir <ozcan1672@outlook.com>
2023-07-18 22:06:32 +0300

    .

---
+++ Assets/Scripts/app.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class app : MonoBehaviour
+{
+    [SerializeField] int value1, value2;
+    List<int> allValues = new List<int>();
+    List<int> divisibleByTwo = new List<int>();
+    List<int> divisibleByThree = new List<int>();
+    List<int> divisibleByFour = new List<int>();
+    List<int> divisibleByFive = new List<int>();
+
+    private void Start()
+    {
+        for (int i = value1; i <= value2; i++)
+        {
+            allValues.Add(i);
+            if (i % 2 == 0)
+                divisibleByTwo.Add(i);
+            if (i % 3 == 0)
+                divisibleByThree.Add(i);
+            if (i % 4 == 0)
+                divisibleByFour.Add(i);
+            if (i % 5 == 0)
+                divisibleByFive.Add(i);
+        }
+
+        writer(allValues);
+        writer(divisibleByTwo);
+        writer(divisibleByThree);
+        writer(divisibleByFour);
+        writer(divisibleByFive);
+    }
+
+    void writer(List<int> list)
+    {
+        foreach (var item in list)
+        {
+            print(item);
+        }
+    }
+}
